b020dc6cab110952d52bb6e17d42f42952fd6b8a
appleshakey <adarshnikhil181@gmail.com>
2024-06-13 02:40:17 +0530

    Added all the pages for mobile and included the navbar also

--- src/app/schedule/page.js
+++ src/app/schedule/page.js
@@ -1,32 +1,332 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useMediaQuery } from "@mui/material";
 export default function Schedule(){
     const router = useRouter();
+    const isMobileScreen = useMediaQuery('(min-width: 1000px)')
     return (
-        <div className="text-white font-white h-[100vh] w-[100vw] flex items-center justify-center px-[82px] gap-24 ">
-            <button className="flex flex-col text-white font-staat gap-7 items-center" onClick={() => router.push("/schedule/day1")}>
-                <div className="rounded-full bg-gray-300 h-[16vh] w-[16vh]">
+        <div>
+        {isMobileScreen ? (
+            <div className="text-white font-white h-[100vh] w-[100vw] flex items-center justify-center px-[82px] gap-24 ">
+                <button className="flex flex-col text-white font-staat gap-7 items-center" onClick={() => router.push("/schedule/day1")}>
+                    <div className="rounded-full bg-gray-300 h-[16vh] w-[16vh]">
+                    </div>
+                    <div className="text-3xl text-center">
+                        <h2>Day 1</h2>
+                    </div>
+                </button>
+                <button className="flex flex-col text-white font-staat gap-7 items-center"
+                    onClick={() => router.push("/schedule/day2")}
+                >
+                    <div className="rounded-full bg-gray-300 h-[16vh] w-[16vh]">
+                    </div>
+                    <div className="text-3xl text-center">
+                        <h2>Day 2</h2>
+                    </div>
+                </button>
+                <button className="flex flex-col text-white font-staat gap-7 items-center" onClick={() => router.push("/schedule/day3")}>
+                    <div className="rounded-full bg-gray-300 h-[16vh] w-[16vh]">
+                    </div>
+                    <div className="text-3xl text-center">
+                        <h2>Day 3</h2>
+                    </div>
+                </button>
+            </div>
+        ) : (
+            <div className="text-white font-staat py-32 px-6 flex flex-col gap-44 -z-50">
+                <div className="flex flex-col">
+                    <div className="flex gap-5 absolute">
+                        <div className="rounded-full bg-gradient-to-b from-[#9c15b2] to-[#00334d] h-[4vh] w-[4vh] z-10"></div>
+                        <div className="text-3xl">
+                            <h1>Day 1</h1>
+                        </div>
+                    </div>
+                    <div className="h-[10vh] flex flex-col relative top-3">
+                        <div className="w-[4vh] flex justify-center">
+                            <div className="border-l-2 border-white h-[10vh] -z-10">
+                            </div>
+                        </div>
+                        <div>
+                            <div className="flex gap-2">
+                                <div className="w-[4vh] flex justify-center z-10">
+                                    <div className="rounded-full w-[3vh] h-[3vh] bg-gradient-to-t from-[#00a6fb] to-[#00334d] absolute">
+
+                                    </div>
+                                </div>
+                                <div className="flex justify-between w-[75vw] ">
+                                    <div className="flex flex-col justify-center">
+                                        <div className="text-2xl font-semibold">
+                                            <h1>Registration HelpDesk</h1>
+                                        </div>
+                                        <div className="text-lg">
+                                            <p>Venue: Auditorium</p>
+                                        </div>
+                                    </div>
+                                    <div className="flex items-center text-nim_ticket_b p-3">
+                                        Aug 26, 2023
+                                        10: 00Am
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="h-[15vh] flex flex-col relative top-6">
+                        <div className="w-[4vh] flex justify-center">
+                            <div className="border-l-2 border-white h-[15vh]">
+                            </div>
+                        </div>
+                        <div>
+                            <div className="flex gap-2">
+                                <div className="w-[4vh] flex justify-center">
+                                    <div className="rounded-full w-[3vh] h-[3vh] bg-gradient-to-t from-[#00a6fb] to-[#00334d] absolute z-10">
+
+                                    </div>
+                                </div>
+                                <div className="flex justify-between w-[75vw]">
+                                    <div className="flex flex-col justify-center">
+                                        <div className="text-2xl font-semibold">
+                                            <h1>Registration HelpDesk</h1>
+                                        </div>
+                                        <div className="text-lg">
+                                            <p>Venue: Auditorium</p>
+                                        </div>
+                                    </div>
+                                    <div className="flex items-center text-nim_ticket_b p-3">
+                                        Aug 26, 2023
+                                        10: 00Am
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="h-[15vh] flex flex-col relative top-6">
+                        <div className="w-[4vh] flex justify-center">
+                            <div className="border-l-2 border-white h-[15vh]">
+                            </div>
+                        </div>
+                        <div>
+                            <div className="flex gap-2">
+                                <div className="w-[4vh] flex justify-center">
+                                    <div className="rounded-full w-[3vh] h-[3vh] bg-gradient-to-t from-[#00a6fb] to-[#00334d] absolute z-10">
+
+                                    </div>
+                                </div>
+                                <div className="flex justify-between w-[75vw]">
+                                    <div className="flex flex-col justify-center">
+                                        <div className="text-2xl font-semibold">
+                                            <h1>Registration HelpDesk</h1>
+                                        </div>
+                                        <div className="text-lg">
+                                            <p>Venue: Auditorium</p>
+                                        </div>
+                                    </div>
+                                    <div className="flex items-center text-nim_ticket_b p-3">
+                                        Aug 26, 2023
+                                        10: 00Am
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="text-3xl text-center">
-                    <h2>Day 1</h2>
+                <div className="flex flex-col">
+                    <div className="flex gap-5 absolute">
+                        <div className="rounded-full bg-gradient-to-b from-[#9c15b2] to-[#00334d] h-[4vh] w-[4vh] z-10"></div>
+                        <div className="text-3xl">
+                            <h1>Day 2</h1>
+                        </div>
+                    </div>
+                    <div className="h-[10vh] flex flex-col relative top-3">
+                        <div className="w-[4vh] flex justify-center">
+                            <div className="border-l-2 border-white h-[10vh]">
+                            </div>
+                        </div>
+                        <div>
+                            <div className="flex gap-2">
+                                <div className="w-[4vh] flex justify-center z-10">
+                                    <div className="rounded-full w-[3vh] h-[3vh] bg-gradient-to-t from-[#00a6fb] to-[#00334d] absolute">
+
+                                    </div>
+                                </div>
+                                <div className="flex justify-between w-[75vw]">
+                                    <div className="flex flex-col justify-center">
+                                        <div className="text-2xl font-semibold">
+                                            <h1>Registration HelpDesk</h1>
+                                        </div>
+                                        <div className="text-lg">
+                                            <p>Venue: Auditorium</p>
+                                        </div>
+                                    </div>
+                                    <div className="flex items-center text-nim_ticket_b p-3">
+                                        Aug 26, 2023
+                                        10: 00Am
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="h-[15vh] flex flex-col relative top-6">
+                        <div className="w-[4vh] flex justify-center">
+                            <div className="border-l-2 border-white h-[15vh]">
+                            </div>
+                        </div>
+                        <div>
+                            <div className="flex gap-2">
+                                <div className="w-[4vh] flex justify-center z-10">
+                                    <div className="rounded-full w-[3vh] h-[3vh] bg-gradient-to-t from-[#00a6fb] to-[#00334d] absolute">
+
+                                    </div>
+                                </div>
+                                <div className="flex justify-between w-[75vw]">
+                                    <div className="flex flex-col justify-center">
+                                        <div className="text-2xl font-semibold">
+                                            <h1>Registration HelpDesk</h1>
+                                        </div>
+                                        <div className="text-lg">
+                                            <p>Venue: Auditorium</p>
+                                        </div>
+                                    </div>
+                                    <div className="flex items-center text-nim_ticket_b p-3">
+                                        Aug 26, 2023
+                                        10: 00Am
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="h-[15vh] flex flex-col relative top-6">
+                        <div className="w-[4vh] flex justify-center">
+                            <div className="border-l-2 border-white h-[15vh]">
+                            </div>
+                        </div>
+                        <div>
+                            <div className="flex gap-2">
+                                <div className="w-[4vh] flex justify-center z-10">
+                                    <div className="rounded-full w-[3vh] h-[3vh] bg-gradient-to-t from-[#00a6fb] to-[#00334d] absolute">
+
+                                    </div>
+                                </div>
+                                <div className="flex justify-between w-[75vw]">
+                                    <div className="flex flex-col justify-center">
+                                        <div className="text-2xl font-semibold">
+                                            <h1>Registration HelpDesk</h1>
+                                        </div>
+                                        <div className="text-lg">
+                                            <p>Venue: Auditorium</p>
+                                        </div>
+                                    </div>
+                                    <div className="flex items-center text-nim_ticket_b p-3">
+                                        Aug 26, 2023
+                                        10: 00Am
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </button>
-            <button className="flex flex-col text-white font-staat gap-7 items-center"
-                onClick={() => router.push("/schedule/day2")}
-            >
-                <div className="rounded-full bg-gray-300 h-[16vh] w-[16vh]">
+                <div className="flex flex-col">
+                    <div className="flex gap-5 absolute">
+                        <div className="rounded-full bg-gradient-to-b from-[#9c15b2] to-[#00334d] h-[4vh] w-[4vh] z-10"></div>
+                        <div className="text-3xl">
+                            <h1>Day 3</h1>
+                        </div>
+                    </div>
+                    <div className="h-[10vh] flex flex-col relative top-3">
+                        <div className="w-[4vh] flex justify-center">
+                            <div className="border-l-2 border-white h-[10vh]">
+                            </div>
+                        </div>
+                        <div>
+                            <div className="flex gap-2">
+                                <div className="w-[4vh] flex justify-center">
+                                    <div className="rounded-full w-[3vh] h-[3vh] bg-gradient-to-t from-[#00a6fb] to-[#00334d] absolute z-10">
+
+                                    </div>
+                                </div>
+                                <div className="flex justify-between w-[75vw]">
+                                    <div className="flex flex-col justify-center">
+                                        <div className="text-2xl font-semibold">
+                                            <h1>Registration HelpDesk</h1>
+                                        </div>
+                                        <div className="text-lg">
+                                            <p>Venue: Auditorium</p>
+                                        </div>
+                                    </div>
+                                    <div className="flex items-center text-nim_ticket_b p-3">
+                                        Aug 26, 2023
+                                        10: 00Am
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="h-[15vh] flex flex-col relative top-6">
+                        <div className="w-[4vh] flex justify-center">
+                            <div className="border-l-2 border-white h-[15vh]">
+                            </div>
+                        </div>
+                        <div>
+                        <div>
+                            <div className="flex gap-2">
+                                <div className="w-[4vh] flex justify-center z-10">
+                                    <div className="rounded-full w-[3vh] h-[3vh] bg-gradient-to-t from-[#00a6fb] to-[#00334d] absolute">
+
+                                    </div>
+                                </div>
+                                <div className="flex justify-between w-[75vw]">
+                                    <div className="flex flex-col justify-center">
+                                        <div className="text-2xl font-semibold">
+                                            <h1>Registration HelpDesk</h1>
+                                        </div>
+                                        <div className="text-lg">
+                                            <p>Venue: Auditorium</p>
+                                        </div>
+                                    </div>
+                                    <div className="flex items-center text-nim_ticket_b p-3">
+                                        Aug 26, 2023
+                                        10: 00Am
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="h-[15vh] flex flex-col relative top-6">
+                        <div className="w-[4vh] flex justify-center">
+                            <div className="border-l-2 border-white h-[15vh]">
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <div className="flex gap-2">
+                                    <div className="w-[4vh] flex justify-center z-10">
+                                        <div className="rounded-full w-[3vh] h-[3vh] bg-gradient-to-t from-[#00a6fb] to-[#00334d] absolute">
+
+                                        </div>
+                                    </div>
+                                    <div className="flex justify-between w-[75vw]">
+                                        <div className="flex flex-col justify-center">
+                                            <div className="text-2xl font-semibold">
+                                                <h1>Registration HelpDesk</h1>
+                                            </div>
+                                            <div className="text-lg">
+                                                <p>Venue: Auditorium</p>
+                                            </div>
+                                        </div>
+                                        <div className="flex items-center text-nim_ticket_b p-3">
+                                            Aug 26, 2023
+                                            10: 00Am
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="text-3xl text-center">
-                    <h2>Day 2</h2>
-                </div>
-            </button>
-            <button className="flex flex-col text-white font-staat gap-7 items-center" onClick={() => router.push("/schedule/day3")}>
-                <div className="rounded-full bg-gray-300 h-[16vh] w-[16vh]">
-                </div>
-                <div className="text-3xl text-center">
-                    <h2>Day 3</h2>
-                </div>
-            </button>
+            </div>
+        )}
+            
         </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,38 @@
 import { useParallax } from "react-scroll-parallax"
 import { useRouter } from "next/navigation";
 import { useMediaQuery } from "@mui/material";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 export default function Navbar(){
     const router = useRouter();
+    const [MNavbar, setMNavbar] = useState(false);
     const isMobileScreen = useMediaQuery('(min-width: 1000px)');
+    const menuVariant = {
+        "open": {
+            scale: 1,
+            transition: {
+                delayChildren: 0.3,
+                staggerChildren: 0.2
+              }
+        },
+        "close": {
+            scale: 0,
+        }
+    }
+
+    const menuItemVariant = {
+        "open": {
+            y:0,
+            opacity: 1,
+        },
+        "close": {
+            y: -100,
+            opacity: 0,
+        }
+    }
     return(
-    <div>
+    <motion.div>
         {isMobileScreen ? (
         <div className="text-white font-staat text-2xl px-[82px] flex justify-between py-3 items-center fixed w-full z-50">
             <div>
@@ -23,18 +49,45 @@ export default function Navbar(){
             <button className="bg-[#5cb1f6] opacity-80 px-3 py-1 text-md rounded-md">register</button>
         </div>
         ) : (
-            <div className="flex justify-between p-3 items-center fixed w-[100vw]">
+            <motion.div className="flex justify-between p-3 items-center fixed w-[100vw] z-50">
                 <div>
                     <img src="/csis_.png" className="h-14 w-32"/>
                 </div>
-                <button>
+                <button onClick={() => setMNavbar(!MNavbar)}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="h-[40px] w-[40px]">
                         <path fill="#ffffff" d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/>
                     </svg>
                 </button>
-            </div>
+                <motion.div className="fixed h-[100vh] w-[100vw] left-0 top-0 flex justify-center items-center z-50 bg-gray-900 bg-opacity-50" animate = { MNavbar ? "open" : "close"} variants={menuVariant}>
+                        <motion.div className="text-white text-3xl h-[50vh] w-[75vw] bg-gradient-to-t from-iem_ticket_b to-iem_ticket_t rounded-lg p-5 flex flex-col gap-5 z-50">
+                            <motion.div className="flex justify-end font-staat">
+                                <button onClick={() => setMNavbar(!MNavbar)}>X</button>
+                            </motion.div>
+                            <motion.div className="text-4xl font-staat flex justify-center" variants={menuItemVariant}>
+                                <h1>Menu</h1>
+                            </motion.div>
+                            <motion.div className="flex flex-col items-center font-staat gap-5" variants={menuVariant}>
+                                <motion.button onClick={() => {
+                                    setMNavbar(!MNavbar);
+                                    router.push('/');}} variants={menuItemVariant}>Home</motion.button>
+                                <motion.button onClick={() => {
+                                    setMNavbar(!MNavbar);
+                                    router.push('/schedule');}} variants={menuItemVariant}>Schedule</motion.button>
+                                <motion.button onClick={() => {
+                                    setMNavbar(!MNavbar);
+                                    router.push('/events');}} variants={menuItemVariant}>Events</motion.button>
+                                <motion.button onClick={() => {
+                                    setMNavbar(!MNavbar);
+                                    router.push('/speakers');}} variants={menuItemVariant}>Speakers</motion.button>
+                                <motion.button onClick={() => {
+                                    setMNavbar(!MNavbar);
+                                    router.push('/');}} variants={menuItemVariant}>Register Now</motion.button>
+                            </motion.div>
+                        </motion.div>
+                    </motion.div>
+            </motion.div>
         )}
         
-    </div>
+    </motion.div>
     )
 }
